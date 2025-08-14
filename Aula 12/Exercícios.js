819e472574df5c8be3fe5cce0f572abf0c75d2ec
Carol-Schmitt <nina.fschmitt@hotmail.com>
2025-08-14 14:44:29 -0300

    Create Exercícios.js

---
+++ Aula 12/Exercícios.js
@@ -0,0 +1,9 @@
+// Arrays are a way to store multiple values in a single variable.
+// They can hold any type of data, including numbers, strings, and even other arrays.
+ 
+let thingsILike = ["Pizza", "Movies", "Music", "Chocolate", "Dance"];
+    console.log(thingsILike.pop())
+    console.log(thingsILike.push())
+    console.log(thingsILike.unshift())
+    console.log(thingsILike.shift())
+// There are many methods to manipulate arrays.
