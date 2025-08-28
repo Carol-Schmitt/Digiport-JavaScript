cee9e64724c9bb22839cf3d9f8ec01a1ffce87d8
Carol-Schmitt <nina.fschmitt@hotmail.com>
2025-08-28 15:11:56 -0300

    Aula 16 - Conceitos

---
+++ Aula 16/Conceitos.js
@@ -0,0 +1,5 @@
+//instanciar = criar um novo objeto para uma classe quando há um 'constructor' dentro da classe. 
+
+//quando a variavel é privada, a gente coloca uma # antes do atributo (exemplo: conta de banco)
+
+//heranças: usa 'Extends'
